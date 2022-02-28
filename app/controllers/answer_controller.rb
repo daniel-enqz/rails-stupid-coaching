5a697de44fff696767ce5a69ffd07932e7cba5e3
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-28 12:25:53 -0600

    rails new

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,4 +1,13 @@
 class AnswerController < ApplicationController
   def answer
+    @question = params[:question]
+
+    @answer = if @question.include?('?')
+                'Silly question, get dressed and go to work!.'
+              elsif @question == 'I am going to work'
+                'GREAT!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
