098be1ef168d8221569e8d8fc188f9bea60bb74e
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-28 12:53:28 -0600

    files added

---
+++ test/system/questions_test.rb
@@ -0,0 +1,11 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello, pana"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+end
